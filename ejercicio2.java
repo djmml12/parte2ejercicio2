3a476442eadb32ef6b57e52fdc75e492c72d7941
djmml12 <39584961+djmml12@users.noreply.github.com>
2021-06-10 15:55:41 -0600

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ejercicio2{
+    public static void main(String[] args){ 
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        System.out.println("ingresar número entero");
+        num = scanner.nextInt();
+    for (int i=num; i>=1; i--){
+        System.out.println(""+i);
+    }
+
+    }
+
+} 
